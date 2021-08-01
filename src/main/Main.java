e021c8712a0b8896d51c7ce2d809a44aa8d8a00c
swh <sungwoo166@gmail.com>
2021-08-01 23:38:09 +0900

    .
    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,12 +12,13 @@ public class Main {
 		
 		
 		
-		
 	}
 	public static void main(String[] args) {
 		
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
+		
+		
 		System.out.println(func(n));
         scn.close();
 	}
